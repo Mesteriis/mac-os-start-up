0a6320144f54c8eb2a1355033d7552e77c06de34
Alexander Mescheryakov <avm@sh-inc.ru>
2024-06-13 14:35:02 +0200

    init

---
+++ brew.sh
@@ -0,0 +1,4 @@
+# Description: This script installs Homebrew on macOS.
+
+@echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
---
+++ quicklook-plugins.sh
@@ -0,0 +1,16 @@
+@brew install --cask qlcolorcode
+
+
+@brew install --cask betterzip
+@brew install --cask gltfquicklook
+# https://code.google.com/archive/p/cert-quicklook/
+@brew install --cask certquicklook
+# https://github.com/anthonygelibert/QLColorCode
+@brew install --cask QLColorCode
+# https://github.com/whomwah/qlstephen
+@brew install --cask qlstephen
+@brew install --cask quicklook-json
+@brew install --cask quicklook-csv
+@brew install --cask quicklookapk
+@brew install --cask QLCommonMark
+@brew install --cask ipynb-quicklook
